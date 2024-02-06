0961789ae85975b4e99b5886b120c400999bc5fc
Jan Henk Hazelaar <janhenk@concept7.nl>
2024-02-06 14:01:06 +0100

    Cleanup what we not use

--- src/ComposerHealthServiceProvider.php
+++ src/ComposerHealthServiceProvider.php
@@ -10,16 +10,8 @@ class ComposerHealthServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('composer-health')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_composer-health_table')
-            ->hasCommand(ComposerHealthCommand::class);
+            ->hasConfigFile();
     }
 }
